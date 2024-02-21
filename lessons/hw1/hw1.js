f9ac2b8f95d16a9b234bd3a54a92ff77b0ac7c4f
penetrat0r228 <navigator339312@gmail.com>
2024-02-21 22:04:57 +0200

    Initial commit

---
+++ lessons/hw1/hw1.js
@@ -0,0 +1,47 @@
+//  ----------------task1----------------
+
+let a = 'hello';
+console.log (a);
+let b = 'owu';
+console.log (b);
+let c = 'com';
+console.log (c);
+let d = 'ua';
+console.log (d);
+let e = 1;
+console.log (e);
+let f = 10;
+console.log (f);
+let g = -999;
+console.log (g);
+let h = 123;
+console.log (h);
+let i = 3.14;
+console.log (i);
+let j = 2.7;
+console.log (j);
+let k = 16;
+console.log (k);
+let l = true;
+console.log (l);
+let m = false;
+console.log (m);
+
+// -----------------task2-----------------
+
+let firstName = 'Ivan';
+let middleName = 'Mikhailovich';
+let lastName = 'Kinash';
+let person = `${firstName} ${middleName} ${lastName}`;
+console.log(person);
+
+// -----------------task3-----------------
+
+let n = 100;
+let o = '100';
+let p = true;
+console.log(typeof n);
+console.log(typeof o);
+console.log(typeof p);
+
+
---
+++ lessons/lesson1/main.js
@@ -0,0 +1,46 @@
+console.log('gfrsgsgs')
+// // declaration
+// var a;
+// // string boolean number
+// // init
+// a = 'jdfjsfngks';
+// a = 35151.5325;
+// a = false
+//
+// var b = true;
+//
+// let a = 'okten';
+// console.log(a);
+// a = 100500;
+// console.log(a);
+
+// let data = 'okten';
+// console.log(data);
+// console.log(data);
+// console.log(data);
+// console.log(data);
+
+// const c = 'dadadad';
+
+// let w1 = 'okten';
+// let w2 = 'is';
+// let w3 = 'sucks';
+//
+// let sen = w1 + w2 + w3;
+// console.log(sen);
+//
+// let sen2 = `${w1} ${w2} ${w3}`;
+// console.log(sen2);
+
+// let n1 = 10;
+// let n2  = 20;
+// let res1 = n1 + n2;
+// console.log(n1 +n2);
+// console.log(res1);
+//
+// console.log(15 % 2);
+
+// let bool1 = true;
+// let bool2= false;
+// console.log(bool1 + bool2);
+// console.log(10 - true)
